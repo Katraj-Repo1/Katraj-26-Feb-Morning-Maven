864961f395eb158339b402fa14c8641f2508e4ff
shubhamjane <shubhamsjane97@gmail.com>
2022-06-17 14:01:55 +0530

    add one class

---
+++ src/test/java/test/CheckPrintLine.java
@@ -0,0 +1,12 @@
+package test;
+
+public class CheckPrintLine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi");
+System.out.println("hi hello");
+System.out.println("");
+	}
+
+}
